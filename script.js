31c836909b2daeb8acbb739ffbac83118a814e5e
Yash Vaidya <bobbynaldocr7@gmail.com>
2024-09-02 22:19:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//your JS code here. If required.
+
 const buttons = document.querySelectorAll('.btn:not(.stop)');
 const stopButton = document.querySelector('.stop');
 let currentAudio;
@@ -14,7 +14,14 @@ stopButton.addEventListener('click', stopSound);
 
 function playSound(sound) {
     stopSound(); 
-    currentAudio = new Audio(`./sounds/${sound}.mp3`);
+
+
+    currentAudio = document.createElement('audio');
+    currentAudio.src = `./sounds/${sound}.mp3`;
+    currentAudio.setAttribute('controls', 'controls'); 
+    currentAudio.style.display = 'none'; 
+    document.body.appendChild(currentAudio);
+
     currentAudio.play();
 }
 
@@ -22,5 +29,6 @@ function stopSound() {
     if (currentAudio) {
         currentAudio.pause();
         currentAudio.currentTime = 0;
+        currentAudio.remove(); 
     }
 }
